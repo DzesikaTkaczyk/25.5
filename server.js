256bfa9223461c69925e9798b56b930bbd4fcecf
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-10-22 11:21:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store');
@@ -10,5 +10,5 @@ app.get('/store', function (req, res) {
     res.send('To jest sklep');
 });
 
-var server = app.listen(3000);
+const server = app.listen(3000);
 
